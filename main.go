98607b890fb77adff04f00bea02974028e592668
Michail Borisenko <bmaa@yandex.ru>
2024-10-23 08:12:31 +0300

    241023 home

--- main.go
+++ main.go
@@ -3,27 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	intervals := [][]int{{1, 3}, {6, 9}}
-	interval := []int{2, 5}
+	intervals := [][]int{{1, 5}, {17, 19}}
+	//	interval := []int{2, 5}
+	interval := []int{2, 4}
 	fmt.Println(insert(intervals, interval))
 }
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func insert(intervals [][]int, newInterval []int) [][]int {
-	// правая граница i дожна быть быть больше или равна левой границы ni
-	// перед или на каком отрезке каким отрезком начинается новый отрезок
-	i := 0
-	for intervals[i][1] < newInterval[0] {
-		i++
-	}
-	// перед каким заканчивается
-	j := 0
-	for intervals[j][0] < newInterval[1] {
-		j++
-	}
-	// i == j
-	// ni[1] < i[i][0] - вставить ni перед i[i] и выйти
-	// иначе
-	// в i[i][0] = min(i[i][0],ni[0])
-	// в i[i][1] = max(i[i][1],ni[1])
-	// i-й исходный для слияния следующих интервалов
-	return intervals
+	ret := [][]int{}
+	lpi := -1 // индекс отрезка внутри которого левая точка интервала
+	rpi := -1 // индекс отрезка внутри которого правая точка интервала
+	lppi := 0 // индекс отрезка перед которым находится левая точка интервала
+	rppi := 0 // индекс отрезка перед которым находится правая точка
+	return ret
 }
